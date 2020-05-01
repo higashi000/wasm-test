69518b245e0bcce7dbe8e0e308c049ef08d65f53
higashi000 <higashi.ict@gmail.com>
2020-05-02 00:49:42 +0900

    Add kagome

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/higashi000/wasm-test
 go 1.13
 
 require (
+	github.com/ikawaha/kagome v1.11.2
 	github.com/shurcooL/go v0.0.0-20191216061654-b114cc39af9f // indirect
 	github.com/shurcooL/go-goon v0.0.0-20170922171312-37c2f522c041 // indirect
 	github.com/shurcooL/goexec v0.0.0-20181215213458-76a6b9a99126 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"syscall/js"
+
+	"github.com/ikawaha/kagome/tokenizer"
+)
 
 func main() {
-  fmt.Println("Hello")
+	document := js.Global().Get("document")
+	text := document.Call("getElementById", "text")
+	res := document.Call("getElementById", "result")
+
+	t := tokenizer.New()
+	tokens := t.Tokenize(text.Get("value").String())
+
+	for _, token := range tokens {
+		if token.Class == tokenizer.DUMMY {
+			fmt.Printf("%s\n", token.Surface)
+			continue
+		}
+		features := strings.Join(token.Features(), ",")
+		fmt.Printf("%s\t%v\n", token.Surface, features)
+		features = "<p>" + token.Surface + ": " + features + "</p>"
+		res.Call("insertAdjacentHTML", "beforeend", features)
+	}
 }
